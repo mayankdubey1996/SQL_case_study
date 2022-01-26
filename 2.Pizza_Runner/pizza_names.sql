32e0877702f88decef768421a01332b58162512b
Mayank Dubey <39786858+mayankdubey1996@users.noreply.github.com>
2022-01-25 22:20:50 -0500

    Add files via upload

---
+++ 2.Pizza_Runner/pizza_names.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS pizza_names;
+CREATE TABLE pizza_names (
+  "pizza_id" INTEGER,
+  "pizza_name" TEXT
+);
+INSERT INTO pizza_names
+  ("pizza_id", "pizza_name")
+VALUES
+  (1, 'Meatlovers'),
+  (2, 'Vegetarian');
---
+++ 2.Pizza_Runner/pizza_recipes.sql
@@ -0,0 +1,10 @@
+DROP TABLE IF EXISTS pizza_recipes;
+CREATE TABLE pizza_recipes (
+  "pizza_id" INTEGER,
+  "toppings" TEXT
+);
+INSERT INTO pizza_recipes
+  ("pizza_id", "toppings")
+VALUES
+  (1, '1, 2, 3, 4, 5, 6, 8, 10'),
+  (2, '4, 6, 7, 9, 11, 12');
